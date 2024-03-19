41fb8778b443c43037b7017ed1e6329b395a44fd
IwishyouI <assameasyou11@gmail.com>
2024-03-19 20:00:24 +0900

    Axios CORS 해결 및 데이터 서버에서 받아오기

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,5 +1,26 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import axios from 'axios';
+
+const posts = ref([]);
+
+axios.get("/api/posts?page=1&size=5").then((response) => {
+    response.data.forEach((r: any) => {
+        posts.value.push(r);
+    })
+});
+
 </script>
 
 <template>
+    <ul>
+        <li v-for="post in posts" :key="post.id">
+            <div>
+                {{ post.title }}
+            </div>
+
+            <div>{{ post.content }}</div>
+        </li>
+
+    </ul>
 </template>
